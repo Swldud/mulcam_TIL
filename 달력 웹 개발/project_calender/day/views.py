eab80ae4eedfed2f1c6c865a5177fc35d9ff36de
wldudSim <skek0011@jbnu.ac.kr>
2023-04-03 21:44:53 +0900

    commit 3
    오류 송출 명령 추가, git ignore, requirements 추가

--- 달력 웹 개발/project_calender/day/views.py
+++ 달력 웹 개발/project_calender/day/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_GET, require_POST, require_safe
 from .models import Article
 from .forms import ArticleForm
 
@@ -25,7 +24,9 @@ from .forms import ArticleForm
 
 #     # return render(request, 'day/new.html')
 #     return redirect('day:detail', article.pk)
-    
+
+
+@require_http_methods(['GET', 'POST'])
 def create(request):
     if request.method == 'GET':
         form = ArticleForm()
@@ -36,11 +37,10 @@ def create(request):
             article = form.save()
             return redirect('day:detail', article.pk)
         
-    return render(request, 'day/create.html', {
-        'form': form
-    })
+    return render(request, 'day/create.html', {'form': form})
 
 
+@require_safe
 def index(request):
     articles = Article.objects.all()
 
@@ -50,16 +50,20 @@ def index(request):
 
 # article_pk: var rounting으로 넘어온 값
 # article_pk는 당연히 내가 urls에 작성한 변수명임. (변수= 이름을 언제든 내 마음대로 바꿀 수 있다. 단 짝을 잘 맞춰라!)
+
+@require_safe
 def detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk = article_pk)
 
     return render(request, 'day/detail.html', {'article': article})
     # 얘는 변수 딱 하나만 부르는 거니까 article
 
 
-
+@require_safe
 def delete(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk = article_pk)
     article.delete()
     return redirect('day:index')
     
@@ -80,8 +84,12 @@ def delete(request, article_pk):
 #     article.save()
 #     return redirect('day:detail', article.pk)
 
+
+@require_http_methods(['GET', 'POST'])
 def edit(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk = article_pk)
+
     if request.method == 'GET':
         form = ArticleForm(instance=article)
         
